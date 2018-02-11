a48f0e85fb684027a0307a05dc6f59353ce2689a
lla <lla@htc-cs.ru>
2018-02-11 15:06:38 +0400

    add material

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Container from 'muicss/lib/react/container';
 
 import PageMain from '../page-main';
 import PageSub from '../page-sub';
 
-import './app.css';
-
 export default class App extends React.Component {
     render() {
         return (
-            <Switch>
-                <Route
-                    exact={ true }
-                    path='/'
-                    component={ PageMain }
-                />
-                <Route
-                    path='*'
-                    component={ PageSub }
-                />
-            </Switch>
+            <Container>
+                <Switch>
+                    <Route
+                        exact={ true }
+                        path='/'
+                        component={ PageMain }
+                    />
+                    <Route
+                        path='*'
+                        component={ PageSub }
+                    />
+                </Switch>
+            </Container>
         );
     }
 }
